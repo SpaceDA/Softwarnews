28eaf5e687b4d7e96cd064e35d7a09a5ff209d42
john <john@spaceda.space>
2022-02-20 16:00:55 -0500

    update config to production

--- application/__init__.py
+++ application/__init__.py
@@ -14,7 +14,7 @@ login_manager = LoginManager()
 def init_app():
     """Initialize the core application."""
     app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.ProgConfig')
+    app.config.from_object('config.ProdConfig')
 
     db.init_app(app)
 
