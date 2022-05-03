01e25ba3215973108230116f9d57188372fc8f9a
john <john@spaceda.space>
2022-05-03 17:06:12 -0400

    change config to prod

--- softwarnews/__init__.py
+++ softwarnews/__init__.py
@@ -15,7 +15,7 @@ def init_app():
     """Initialize the core application."""
     app = Flask(__name__, instance_relative_config=False)
 
-    app.config.from_object('config.DevConfig')
+    app.config.from_object('config.ProdConfig')
     db.init_app(app)
 
 
